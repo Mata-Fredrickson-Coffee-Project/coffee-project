3ed596d45d26d1941e00b3f8997b08b779ce2476
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-18 16:59:13 -0500

    finished all JS functionality and some styling

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
 "use strict";
-
+// --------------------------Function to create coffee Cards based on Array------------
 function renderCoffee(coffee) {
-  var html = '<div class="coffee card m-3">';
+  let html =
+    '<div class="coffee card m-3 shadow-lg border border-primary-subtle">';
   html += "<div class='card-body'>";
   html += "<h4 class='card-title'>" + coffee.name + "</h4>";
   html += "<p>" + coffee.roast + "</p>";
-  html += "<div class='form-check'>";
+  html += "<div class='form-check form-check-inline'>";
   html +=
-    '<input class="form-check-input" type="checkbox" value="" id="addToCartCheck">\n' +
-    '  <label class="form-check-label" for="flexCheckDefault">\n' +
-    "    Add to Cart\n" +
-    "  </label>";
+    '<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio1" value="option1">';
+  html += '<label class="form-check-label" for="inlineRadio1">8oz Bag</label>';
+  html += "</div>";
+  html += "<div class='form-check form-check-inline'>";
+  html +=
+    '<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio2" value="option1">';
+  html += '<label class="form-check-label" for="inlineRadio1">16oz Bag</label>';
+  html += "</div>";
+  html += "<div class='form-check form-check-inline'>";
+  html +=
+    '<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio1" value="option1">';
+  html += '<label class="form-check-label" for="inlineRadio1">32oz Bag</label>';
+  html += "</div>";
+  html += "<div>";
+  html +=
+    '<label for="roast-selection-1"></label>\n' +
+    '                      <select class="form-select" id="roast-selection-1">\n' +
+    "                        <option selected>Choose Grind Type.</option>\n" +
+    "                        <option>Coarse</option>\n" +
+    "                        <option>Medium-Coarse</option>\n" +
+    "                        <option>Medium</option>\n" +
+    "                        <option>Fine</option>\n" +
+    "                        <option>Extra Fine</option>\n" +
+    "                        <option>Whole Bean</option>\n" +
+    "                      </select>";
 
+  html += "</div>";
+  html += "</div>";
   html += "</div>";
   html += "</div>";
   html += "</div>";
   return html;
 }
 
+// -------------------------Function to Render coffee on page/array item------------
+
 function renderCoffees(coffees) {
-  var html = "";
-  for (var i = coffees.length - 1; i >= 0; i--) {
+  let html = "";
+  for (let i = coffees.length - 1; i >= 0; i--) {
     html += renderCoffee(coffees[i]);
   }
   return html;
 }
+
+// -----------------Function for top form to sort Coffee-----------
 
 function updateCoffees(e) {
   e.preventDefault(); // don't submit the form, we just want to update the data
@@ -37,6 +65,22 @@ function updateCoffees(e) {
   });
   coffeeDisplay.innerHTML = renderCoffees(filteredCoffees);
 }
+
+let coffeeInput = document.querySelector("#coffee-data-list");
+// -------------------Function to Display Typed Coffee ------------------
+function displayCoffeeInput() {
+  let newArr = [];
+  coffees.forEach(function (coffee) {
+    if (coffee.name.startsWith(coffeeInput.value)) {
+      newArr.push(coffee);
+    }
+  });
+  coffeeDisplay.innerHTML = renderCoffees(newArr);
+}
+
+coffeeInput.addEventListener("keyup", displayCoffeeInput);
+
+// ----------------Function to Add Custom Coffee------------------
 
 function addCustomCoffee(e) {
   e.preventDefault(); // don't submit the form, we just want to update the data
@@ -51,6 +95,8 @@ function addCustomCoffee(e) {
 
   coffeeDisplay.innerHTML = renderCoffees(coffees);
 }
+
+// -----------------------Array of Coffees-----------------
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -70,12 +116,24 @@ var coffees = [
   { id: 14, name: "French", roast: "Dark Roast" },
 ];
 
-var coffeeDisplay = document.querySelector("#coffee-display");
-var submitButton = document.querySelector("#submit");
-var submitCustomButton = document.querySelector("#submit-custom");
-var roastSelection = document.querySelector("#roast-selection");
-var customRoastSelection = document.querySelector("#custom-roast-selection");
-coffeeDisplay.innerHTML = renderCoffees(coffees);
+// ----------------selectors and eventListeners-----------------
 
-submitButton.addEventListener("click", updateCoffees);
+let coffeeDisplay = document.querySelector("#coffee-display");
+let submitCustomButton = document.querySelector("#submit-custom");
+let roastSelection = document.querySelector("#roast-selection-1");
+let customRoastSelection = document.querySelector("#custom-roast-selection");
+
+let sortedCoffee = coffees.sort((x, y) => {
+  if (x.id > y.id) {
+    return -1;
+  }
+  if (x.id < y.id) {
+    return 1;
+  }
+  return 0;
+});
+
+coffeeDisplay.innerHTML = renderCoffees(sortedCoffee);
+
+roastSelection.addEventListener("change", updateCoffees);
 submitCustomButton.addEventListener("click", addCustomCoffee);
