63926c72255f09324bf8cade52a68c571b97b8c8
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-18 11:05:02 -0500

    fixed form dropdown issue

--- main.js
+++ main.js
@@ -40,20 +40,20 @@ function updateCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-  { id: 1, name: "Light City", roast: "light" },
-  { id: 2, name: "Half City", roast: "light" },
-  { id: 3, name: "Cinnamon", roast: "light" },
-  { id: 4, name: "City", roast: "medium" },
-  { id: 5, name: "American", roast: "medium" },
-  { id: 6, name: "Breakfast", roast: "medium" },
-  { id: 7, name: "High", roast: "dark" },
-  { id: 8, name: "Continental", roast: "dark" },
-  { id: 9, name: "New Orleans", roast: "dark" },
-  { id: 10, name: "European", roast: "dark" },
-  { id: 11, name: "Espresso", roast: "dark" },
-  { id: 12, name: "Viennese", roast: "dark" },
-  { id: 13, name: "Italian", roast: "dark" },
-  { id: 14, name: "French", roast: "dark" },
+  { id: 1, name: "Light City", roast: "Light Roast" },
+  { id: 2, name: "Half City", roast: "Light Roast" },
+  { id: 3, name: "Cinnamon", roast: "Light Roast" },
+  { id: 4, name: "City", roast: "Medium Roast" },
+  { id: 5, name: "American", roast: "Medium Roast" },
+  { id: 6, name: "Breakfast", roast: "Medium Roast" },
+  { id: 7, name: "High", roast: "Dark Roast" },
+  { id: 8, name: "Continental", roast: "Dark Roast" },
+  { id: 9, name: "New Orleans", roast: "Dark Roast" },
+  { id: 10, name: "European", roast: "Dark Roast" },
+  { id: 11, name: "Espresso", roast: "Dark Roast" },
+  { id: 12, name: "Viennese", roast: "Dark Roast" },
+  { id: 13, name: "Italian", roast: "Dark Roast" },
+  { id: 14, name: "French", roast: "Dark Roast" },
 ];
 
 var coffeeDisplay = document.querySelector("#coffee-display");
